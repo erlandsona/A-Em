db936a760567d75e22f0d5c5649f94d2abcb36e5
Austin Erlandson <austin.erlandson@metova.com>
2016-10-22 00:08:46 -0500

    Touch ups...

--- src/Index.elm
+++ src/Index.elm
@@ -16,32 +16,3 @@ main =
 
 
 fader = node "fader"
-
--- CSS
--- titleLogo =
---   style
---   [ ("color", "white")
---   , ("margin-left", "0.67em")
---   ]
-
--- mainStyle =
---   style
---   [ ("position", "absolute")
---   ]
-
--- backgroundImage =
---   style
---   [ ("background", "url(images/stairs.jpg) no-repeat")
---   , ("background-size", "cover")
---   , ("height", "100vh")
---   , ("width", "100vw")
---   ]
-
-
--- main : Html msg
--- main =
---     Html.div []
---         [ Html.div [ class [ MyCss.NavBar ] ] [ Html.text "this has the navbar class" ]
---         , Html.div [ id MyCss.Page ] [ Html.text "this has the Page id" ]
---         ]
--- Contact GitHub API Training Shop Blog About
