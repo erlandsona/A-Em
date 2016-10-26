3063b446a960219fbd48b95a6570630c5590ee61
Austin Erlandson <austin.erlandson@metova.com>
2016-10-25 22:28:26 -0500

    Updates...

--- src/Styles.elm
+++ src/Styles.elm
@@ -2,25 +2,45 @@ module Styles exposing (css)
 
 -- Libs
 import Css exposing (..)
-import Css.Elements exposing (body)
+import Css.Elements exposing (body, main')
 
 -- Src
 import Colors exposing (..)
 
 
+prop = property
+
 css : Stylesheet
 css = stylesheet
   [ body
-    [ before blackOverlay
-    , backgroundColor purple
-    , margin (px 8)
+    [ margin (px 0)
+    , before blackOverlay
+    , prop "background" "url(assets/images/stairs.jpg) center 37% no-repeat"
+    , prop "background-size" "cover"
+    , width  (vw 100)
+    , height (vh 100)
+    ]
+  , main'
+    [ margin (px 0)
+    , position relative
+    , color yellow
     ]
   ]
 
+
+
+
+
+
+
+-- Hide Implementation Details
+
 blackOverlay =
-  [ backgroundColor purple
-  , height (100|>vh)
-  , width  (100|>vw)
-  , position absolute
+  [ backgroundColor black
+  , height (pct 100)
+  , width  (pct 100)
   , opacity (num 0.7)
+  , display block
+  , position absolute
+  , prop "content" "''"
   ]
