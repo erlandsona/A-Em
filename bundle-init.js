20f3f22cb71db6e3c2dd31a7db8c0051f4486f95
Austin Erlandson <austin.erlandson@metova.com>
2016-11-21 20:46:45 -0600

    First deploy...

---
+++ bundle-init.js
@@ -0,0 +1,6 @@
+(function () {
+  'use strict'
+
+  function ready(fn) {if (document.readyState != 'loading'){fn();} else {document.addEventListener('DOMContentLoaded', function () { fn(); });}}
+  ready(Elm.Caldwell.fullscreen);
+})();
