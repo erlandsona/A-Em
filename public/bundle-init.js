0957e14266d1817fb6e34d28e479210f9242164c
Austin Erlandson <austin.erlandson@metova.com>
2016-11-26 16:14:32 -0600

    remove public directory from master branch.

--- public/bundle-init.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  'use strict'
-
-  function ready(fn) {if (document.readyState != 'loading'){fn();} else {document.addEventListener('DOMContentLoaded', function () { fn(); });}}
-  ready(Elm.Caldwell.fullscreen);
-})();
